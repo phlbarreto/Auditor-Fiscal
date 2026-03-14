f8c07d322a902d638c6e37b04653d19111ad6063
Pedro Barreto <phlbarreto90@gmail.com>
2026-03-14 20:14:08 -0300

    feat: implementa layout

---
+++ components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-sky-300 w-full h-10 elevation-2">
+    <v-btn
+      @click="goHome"
+      class="text-capitalize"
+      variant="plain"
+      :ripple="false">
+      <span class="mx-2 font-bold">Auditor Fiscal</span>
+    </v-btn>
+  </div>
+</template>
+<script setup lang="ts">
+  const router = useRouter();
+  const goHome = () => {
+    router.push("/");
+  };
+</script>
