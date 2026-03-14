11132a35f7b1d30df1688a6483fd7656befdf220
Pedro Barreto <phlbarreto90@gmail.com>
2026-03-14 20:16:14 -0300

    feat: muda backgroundColor da aplicação

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
   runtimeConfig: {
     POSTGRES_URL: process.env.POSTGRES_URL,
+  },
+  app: {
+    head: {
+      bodyAttrs: {
+        style: "background-color: #BAE6FD;",
+      },
+    },
   },
 });
