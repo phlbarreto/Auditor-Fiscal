b8701130466939c2023bf8dd8cd0a10354b029da
Pedro Barreto <phlbarreto90@gmail.com>
2026-04-04 21:15:48 -0300

    feat: implementa loading para fetchUserData

---
+++ components/Spinner/Circle.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-overlay :model-value="loadingSpinner" class="align-center justify-center">
+    <v-progress-circular
+      color="info"
+      size="64"
+      indeterminate></v-progress-circular>
+  </v-overlay>
+</template>
+<script setup lang="ts">
+  const mainStore = useMainStore();
+  const { loadingSpinner } = storeToRefs(mainStore);
+</script>
